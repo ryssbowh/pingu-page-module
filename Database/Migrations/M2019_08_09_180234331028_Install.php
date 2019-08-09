0ba3a9cb3891a4da1de71da3cf68a30fbe887246
Unknown <blondin.boris@gmail.com>
2019-08-09 18:14:21 +0100

    unique migrations/seeds

--- Database/Migrations/M2019_08_09_180234331028_Install.php
+++ Database/Migrations/M2019_08_09_180234331028_Install.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class InstallPage extends Migration
+class M2019_08_09_180234331028_Install extends Migration
 {
     /**
      * Run the migrations.
